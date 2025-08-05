a0538c75ba90295000b08c38cd4c656c642ab3fd
davide-delgiudice <dav.del.giudice@gmail.com>
2025-08-05 21:55:44 +0200

    [UPD] create new error after first fetch

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ async function fetchJson(url){
 };
 
 async function getChefBirthday(id){
-    try {
+    try{
         const recipe = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+
+        if(recipe.message){
+            throw new Error(recipe.message);
+        }
+        
         const user = await fetchJson(`https://dummyjson.com/users/${recipe.userId}`);
         return user.birthDate;
     }catch(error){
         console.error(error)
-    }finally{
-        console.log("Fine")
     }
 }
 
-getChefBirthday(1)
-    .then(birthday => console.log("Compleanno:", birthday))
-    .catch(error => console.error(error))
+// getChefBirthday(1)
+//     .then(birthday => console.log("Compleanno:", birthday))
+//     .catch(error => console.error(error))
+
+// nuovo metodo
+(async() => {
+    const birthday = await getChefBirthday(1);
+    console.log('Compeanno Chef:', birthday);
+    console.log('Fine');
+})();
